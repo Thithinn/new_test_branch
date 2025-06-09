5c2573b3f7078827c1d97ee374fda7039d6370d0
Thithinn <Thirumalmaddisettty123@gmail.com>
2025-06-10 00:27:19 +0530

    New Updated

---
+++ cypress.config.js
@@ -0,0 +1,28 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    specPattern: 'cypress/e2e/Test1.js' // n
+  },
+});
+
+// cypress.config.js
+// module.exports = {
+//   e2e: {
+//     specPattern: 'cypress/e2e/**/*Test1.js', // Look for `.cy.js` files in the `e2e` folder
+//     specPattern: 'cypress/e2e/**/*Test2.js', // Look for `.cy.js` files in the `e2e` folder
+//   },
+// };
+module.exports = {
+  e2e: {
+    specPattern: [
+      'cypress/e2e/Test1.js', // First test file
+      'cypress/e2e/Test2.js', // Second test file
+    ],
+     watchForFileChanges: false
+  },
+};
+
